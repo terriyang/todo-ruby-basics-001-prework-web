5f18aedcd231c5cb9e8b4c3426c996c9a19e0a38
terriyang <terrisayshello@gmail.com>
2016-03-06 22:18:42 +0000

    nitrous-backup at Sun Mar  6 22:18:42 UTC 2016

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,17 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
 def argue
-  
+  puts "#{phrase}"
 end
 
 def greeting
-
+  puts "#{greeting}#{name}"
 end
 
 def return_a_value
@@ -22,6 +22,7 @@ def last_evaluated_value
 
 end
 
-def pizza_party
-
+def pizza_party(default= "cheese", pepperoni)
+ puts "#{default}"
+ pepperoni
 end
